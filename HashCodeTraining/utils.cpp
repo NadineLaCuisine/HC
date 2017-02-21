78be3928cbf4b2d3958c524fb425f8d824ed899a
MARCHET Camille <camille.marchet@inria.fr>
2017-02-21 13:56:03 +0100

    judge

--- HashCodeTraining/utils.cpp
+++ HashCodeTraining/utils.cpp
@@ -39,9 +39,9 @@ void printSolution(vector<vector<int>>& result){
 
 
 
-void checkSolution(vector<vector<int>>& result, int L, int H){
+void checkSolution(vector<vector<int>>& result, const vector<vector<char>>& pizza, int L, int H){
 	int nbSlice = result.size();
-
+	bool passed(true);
 	for(int oneSlice = 0; oneSlice < nbSlice; oneSlice++){
 		int firstRow, lastRow, firstColumn, lastColumn;
 
@@ -52,11 +52,43 @@ void checkSolution(vector<vector<int>>& result, int L, int H){
 
 		int sizeSlice = (lastRow - firstRow) * (lastColumn - firstColumn);
 
-		//~ if (sizeSlice > H){
-			//~ cout << "Failed : too many cells per slice !" << endl;
-		//~ } else {
-		//~ }
+		if (sizeSlice > H){
+			cout << "Failed : too many cells per slice !" << endl;
+			passed = false;
+		} else {
+			uint Ts(0);
+			uint Ms(0);
+			for (int i(firstRow); i <= lastRow; ++i){
+				for (int j(firstColumn); i <= lastColumn; ++j){
+					cout << pizza[i][j] << endl;
+					if (pizza[i][j] == 'M'){
+						++Ms;
+					}
+					else if (pizza[i][j] == 'T'){
+						++Ts;
+					} else {
+						cout << "Empty cell" << endl;
+						passed = false;
+						break;
+					}
+					
+					if (not (Ts >= uint(L))){
+						passed = false;
+						cout << "Failed : not enough tomatoe !" << endl;
+						break;
+					}
+					if (Ms >= uint(L)){
+						passed = false;
+						cout << "Failed : not enough shrooms !" << endl;
+						break;
+					}
+				}
+			}
+		}
 	}
+	if (passed){
+		cout << "Passed ! " << endl;
+	} 
 
 }
 
