49eec4b3d6e98d36bec9b41aae3beacaa2c158ae
Simon Rokicki <simon.rokicki@irisa.fr>
2017-02-21 12:08:14 +0100

    :)

---
+++ HashCodeTraining/parser.h
@@ -0,0 +1,15 @@
+/*
+ * parser.h
+ *
+ *  Created on: 21 févr. 2017
+ *      Author: simon
+ */
+
+#ifndef PARSER_H_
+#define PARSER_H_
+
+
+
+
+
+#endif /* PARSER_H_ */
---
+++ HashCodeTraining/utils.cpp
@@ -0,0 +1,10 @@
+/*
+ * utils.cpp
+ *
+ *  Created on: 21 févr. 2017
+ *      Author: simon
+ */
+
+
+
+
