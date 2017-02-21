b0a13a90be8b03a8a3d9ded4c2121f6c054dadc7
MARCHET Camille <camille.marchet@inria.fr>
2017-02-21 12:27:41 +0100

    debut des fonctions

--- HashCodeTraining/parser.cpp
+++ HashCodeTraining/parser.cpp
@@ -4,18 +4,49 @@
  *  Created on: 21 févr. 2017
  *      Author: simon
  */
-#include <thread>
+
+
+
+
+
+#include <vector>
+#include <sstream>
+#include <string>
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <iterator>
-#include <unordered_map>
-#include <unordered_set>
-#include <set>
 #include <algorithm>
-#include <chrono>
-#include <map>
-#include <set>
+#include <unordered_set>
+
+#include "parser.h"
 
 
+readFile(ifstream& readFile, int& R, int& C, int& L, int& H, vector<vector<char>>& pizza){
+	string sequence;
+	vector<string> values;
+	uint i(0);
+	while (not readFile.eof()){
+		i = 0;
+        getline(readFile, sequence);
+        if (not sequence.empty()){
+			value = split(sequence, ' ');
+			R = stoi(value[0]);
+			C = stoi(value[1]);
+			L = stoi(value[2]);
+			H = stoi(value[3]);
+			while (i < R){
+				getline(readFile, sequence);
+			}
+		}
+	}
+}
 
+
+vector<string> split(const string &s, char delim){
+  stringstream ss(s);
+  string item;
+  vector<string> elems;
+  while (getline(ss, item, delim)) {
+    elems.push_back(std::move(item)); 
+  }
+  return elems;
+}
