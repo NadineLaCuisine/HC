ebfd7e7d8816df1a9eaa48ea69fd6a8043c83927
Simon Rokicki <simon.rokicki@irisa.fr>
2017-02-23 20:21:21 +0100

    new main

--- HashCode2017/utils.cpp
+++ HashCode2017/utils.cpp
@@ -24,10 +24,10 @@
 
 void printSolution(FILE* file, vector<vector<int>>& result){
 
-	fprintf(file, "%d\n", result.size());
-	for (int oneServer = 0; oneServer<result.size(); oneServer++){
+	fprintf(file, "%lu\n", result.size());
+	for (unsigned int oneServer = 0; oneServer<result.size(); oneServer++){
 		fprintf(file, "%d ", oneServer);
-		for (int oneVideo = 0; oneVideo<result[oneServer].size(); oneVideo++){
+		for (unsigned int oneVideo = 0; oneVideo<result[oneServer].size(); oneVideo++){
 			fprintf(file, "%d ", result[oneServer][oneVideo]);
 		}
 		fprintf(file, "\n");
@@ -40,9 +40,9 @@ void printSolution(FILE* file, vector<vector<int>>& result){
 
 void checkSolution(vector<vector<int>>& result, vector<int> sizesVideo, int maxSize){
 
-	for (int oneServer = 0; oneServer<result.size(); oneServer++){
+	for (unsigned int oneServer = 0; oneServer<result.size(); oneServer++){
 		int totalSize = 0;
-		for (int oneVideo = 0; oneVideo<result[oneServer].size(); oneVideo++){
+		for (unsigned int oneVideo = 0; oneVideo<result[oneServer].size(); oneVideo++){
 			totalSize += sizesVideo[oneVideo];
 		}
 
@@ -58,23 +58,25 @@ int getScore(vector<vector<int>>& result, vector<struct point>& endPoints){
 	int score = 0;
 
 	//For all endpoint
-	for (int oneEndPoint = 0; oneEndPoint < endPoints.size(); oneEndPoint++){
+	for (unsigned int oneEndPoint = 0; oneEndPoint < endPoints.size(); oneEndPoint++){
 		int latencyToDataCenter = endPoints[oneEndPoint].dataCenterLatency;
 
 		//For all request from this endpoint
-		for (int oneRequest = 0; oneRequest < endPoints[oneEndPoint].idServers.size(); oneRequest++){
+		for (unsigned int oneRequest = 0; oneRequest < endPoints[oneEndPoint].idServers.size(); oneRequest++){
 			int videoID = endPoints[oneEndPoint].videosId[oneRequest];
 			int videoNBRequest = endPoints[oneEndPoint].nbRequests[oneRequest];
 
 			int minLatency = latencyToDataCenter;
-			for (int oneConnectedServer = 0; oneConnectedServer < endPoints[oneEndPoint].idServers.size(); oneConnectedServer++){
+			for (unsigned int oneConnectedServer = 0; oneConnectedServer < endPoints[oneEndPoint].idServers.size(); oneConnectedServer++){
 				int serverId = endPoints[oneEndPoint].idServers[oneConnectedServer];
 				int serverLatency = endPoints[oneEndPoint].latencyToServers[oneConnectedServer];
 
 				bool serverContainsVideo = false;
-				for (int oneVideoInServer = 0; oneVideoInServer<result[serverId].size(); oneVideoInServer++){
-					serverContainsVideo = true;
-					break;
+				for (unsigned int oneVideoInServer = 0; oneVideoInServer<result[serverId].size(); oneVideoInServer++){
+					if (videoID == result[serverId][oneVideoInServer]){
+						serverContainsVideo = true;
+						break;
+					}
 				}
 
 
@@ -89,10 +91,40 @@ int getScore(vector<vector<int>>& result, vector<struct point>& endPoints){
 			score += localScore;
 		}
 	}
+
+	return score;
 }
 
 int getScoreVideoEndPoint(int videoId, int endPointId, vector<struct point>& endPoints){
 
+//	int videoID = endPoints[oneEndPoint].videosId[oneRequest];
+//	int videoNBRequest = endPoints[oneEndPoint].nbRequests[oneRequest];
+//
+//	int minLatency = latencyToDataCenter;
+//	for (unsigned int oneConnectedServer = 0; oneConnectedServer < endPoints[oneEndPoint].idServers.size(); oneConnectedServer++){
+//		int serverId = endPoints[oneEndPoint].idServers[oneConnectedServer];
+//		int serverLatency = endPoints[oneEndPoint].latencyToServers[oneConnectedServer];
+//
+//		bool serverContainsVideo = false;
+//		for (unsigned int oneVideoInServer = 0; oneVideoInServer<result[serverId].size(); oneVideoInServer++){
+//			if (videoID == result[serverId][oneVideoInServer]){
+//				serverContainsVideo = true;
+//				break;
+//			}
+//		}
+//
+//
+//		if (serverContainsVideo)
+//			if (serverLatency<minLatency)
+//				minLatency = serverLatency;
+//
+//	}
+//
+//	//We compute the local score
+//	int localScore = (latencyToDataCenter - minLatency)*1000*videoNBRequest;
+//	score += localScore;
+
+	return 0;
 }
 
 
